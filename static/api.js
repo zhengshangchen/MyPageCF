6a7e3fbbed09ebaee43c84f373fed058a4f8b21c
zhengshangchen <zhengshangchen07@gmail.com>
2026-01-30 21:00:13 +0800

    Update api.js

--- static/api.js
+++ static/api.js
@@ -1,5 +1,5 @@
 // 从全局变量中获取 API 地址，并添加 /api 路径
-const API_BASE_URL = 'https://home.dlldsysplus.asia/api';
+const API_BASE_URL = 'https://nav.3470966893.workers.dev/api';
 let token = null;
 
 // Token 管理
